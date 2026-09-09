2b7258ab8ecc8de245bc43bcbe4354dc2b1bcc34
TEKVP <marikannanponnusamy@zohomail.in>
2026-09-09 10:41:20 +0530

    Add files via upload

---
+++ chat-config.js
@@ -0,0 +1,48 @@
+/* ================================================================
+   TEKTON ELEVATORS
+   CHAT CONFIGURATION
+   ================================================================ */
+
+window.TEKTON_CHAT = {
+
+  mode: 'hybrid',
+
+  proxyUrl:
+    'https://script.google.com/macros/s/AKfycbzyC5W_4RxBC_NTLTnamk_UB-RVk3MHJ4aHMM_amtEiA6lxfVYalA-1GfNqrDt5fQw/exec',
+
+  locationFile:
+    './data/india-locations.json',
+
+  knowledgeFile:
+    './data/tekton-knowledge.json',
+
+  browserAIEnabled: false,
+
+  assistantName:
+    'Tekton Assist',
+
+  assistantRole:
+    'Elevator advisor',
+
+  maxChars:
+    800,
+
+  phone:
+    '+918925448131',
+
+  phoneDisplay:
+    '+91 89254 48131',
+
+  servicePhone:
+    '+919500158530',
+
+  servicePhoneDisplay:
+    '+91 95001 58530',
+
+  email:
+    'info@tektonelevators.com',
+
+  whatsapp:
+    '918925448131'
+
+};
